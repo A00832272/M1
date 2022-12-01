89a8ecd00c5aedb4fcbcd061f65d485d8d323b97
A00832272 <88901347+A00832272@users.noreply.github.com>
2022-11-30 21:00:07 -0600

    Add files via upload

---
+++ M1/agent.py
@@ -0,0 +1,70 @@
+import mesa
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.UserParam import UserSettableParameter
+"""
+"""
+
+class MoneyAgent(mesa.Agent):
+    """An agent with fixed initial wealth."""
+
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos, moore=True, include_center=False, radius=1
+        )
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+    def cleanCell(self):
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        if len(self.pos) == 2:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if(other_agent == TrashAgent):
+                other_agent.destroy += 1
+
+    def step(self):
+        self.cleanCell()
+        self.move()
+
+
+class TrashAgent(mesa.Agent):
+
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.destroy = 0
+
+    def step(self):
+        if (self.destroy >= 1):
+            self.destroy(self)
+
+
+
+
+class MoneyModel(mesa.Model):
+
+    def __init__(self, NumCleaners, NumTrash, width, height):
+        self.boxCoords = [] 
+        self.num_agents = NumCleaners
+        self.num_box = NumTrash
+        self.grid = mesa.space.MultiGrid(width, height, True)
+        self.schedule = mesa.time.RandomActivation(self)
+
+        #setup agents on grid
+        for i in range(self.num_agents):
+            a = MoneyAgent(i, self)
+            self.schedule.add(a)
+            self.grid.place_agent(a, (1, 1))
+
+        #setup boxes
+        for i in range(self.num_box):
+            b = TrashAgent(i + self.num_box, self)
+            self.schedule.add(b)
+            z = self.grid.find_empty()
+            self.boxCoords.append(z)
+            self.grid.place_agent(b, (z))
+
+    def step(self):
+        self.schedule.step()
---
+++ M1/run.py
@@ -0,0 +1,35 @@
+from agent import *
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.UserParam import UserSettableParameter
+import mesa
+
+
+def agent_portrayal(agent):
+
+    portrayal = {"Filled": "true"}
+    if agent.unique_id < 5:
+        portrayal["Shape"] = "circle"
+        portrayal["Color"] = "green"
+        portrayal["Layer"] = 0
+        portrayal["r"] = 0.5
+    else:
+        portrayal["Shape"] = "rect"
+        portrayal["Color"] = "red"
+        portrayal["Layer"] = "1"
+        portrayal["w"] = 0.2
+        portrayal["h"] = 0.2
+        portrayal["xAlign"] = 0.5
+        portrayal["yAlign"] = 0.5
+    return portrayal
+
+grid = mesa.visualization.CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+server = mesa.visualization.ModularServer(
+    MoneyModel, [grid], "Box Model", {"NumCleaners": 5, "NumTrash": 15, "width": 10, "height": 15}
+)
+server = ModularServer(MoneyModel,
+                       [grid],
+                       "Box Model",
+                       {"NumCleaners": 5, "NumTrash": 15, "width": 10, "height": 15})
+server.port = 8520 # The default
+server.launch()
