2a8e877a157a080fabbda44820b064f86ed8ccd9
A00832272 <88901347+A00832272@users.noreply.github.com>
2022-11-30 21:23:44 -0600

    Update run.py

--- M1/run.py
+++ M1/run.py
@@ -25,11 +25,11 @@ def agent_portrayal(agent):
 
 grid = mesa.visualization.CanvasGrid(agent_portrayal, 10, 10, 500, 500)
 server = mesa.visualization.ModularServer(
-    CleaningModel, [grid], "Box Model", {"NumCleaners": 5, "NumTrash": 15, "width": 10, "height": 15}
+    CleaningModel, [grid], "Box Model", {"NumCleaners": 5, "NumTrash": 15, "width": 10, "height": 10}
 )
 server = ModularServer(CleaningModel,
                        [grid],
                        "Box Model",
-                       {"NumCleaners": 5, "NumTrash": 15, "width": 10, "height": 15})
+                       {"NumCleaners": 5, "NumTrash": 15, "width": 10, "height": 10})
 server.port = 8520 # The default
 server.launch()
