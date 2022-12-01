fef8c78a1070b50a7d314336a77ed3c405252337
A00832272 <88901347+A00832272@users.noreply.github.com>
2022-12-01 11:09:53 -0600

    Update agent.py

--- M1/agent.py
+++ M1/agent.py
@@ -1,69 +1,112 @@
 import mesa
-from mesa.visualization.modules import CanvasGrid
-from mesa.visualization.ModularVisualization import ModularServer
-from mesa.visualization.UserParam import UserSettableParameter
-"""
-"""
+import math
 
-class CleanerAgent(mesa.Agent):
+class RobotAgent(mesa.Agent):
 
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
+        self.carrying = 0
+        self.stackCount = 0
+        self.tempClosest = 100
+        self.point = 0,0
+        self.movingToX = 0
+        self.movingToY = 0
+        self.new_pos = (0,0)
+        self.foundBox = False
+
+    def step(self):
+        j = len(self.model.boxCoords)
+        temp = 0
+
+        print("j: ",j)
+        if(j == 0 and (self.pos[0] == 0 and self.pos[1] == 0)):
+            print("1ST")
+            self.stay()
+        else:
+            if (self.carrying == 0):
+                print("2ND")
+                if(self.foundBox == False and j > 0):
+                    for i in range(j):
+                        print(self.model.boxCoords)
+                        if((math.dist(self.pos, self.model.boxCoords[i])) < self.tempClosest):
+                            print(self.tempClosest)
+                            self.tempClosest = math.dist(self.pos, self.model.boxCoords[i])
+
+                            self.point = self.model.boxCoords[i]
+                            temp = i
+                            self.movingToX = self.point[0]
+                            self.movingToY = self.point[1]
+            
+                    print("3rd")
+                    self.foundBox = True
+                    print(temp)
+                    self.model.boxCoords.pop(temp)
+
+            if(self.pos == (self.movingToX, self.movingToY)):
+                print("4TH")
+                self.foundBox = True
+                #self.dropOff()
+        if(self.pos == (0,0)):
+            self.dropOff()
+            self.foundBox =False
+        
+
+        self.move()
+        
+        
 
     def move(self):
-        possible_steps = self.model.grid.get_neighborhood(
-            self.pos, moore=True, include_center=False, radius=1
-        )
-        new_position = self.random.choice(possible_steps)
-        self.model.grid.move_agent(self, new_position)
 
-    def cleanCell(self):
+        #move until you find a box
+        new_pos = 0
+        if(self.carrying == 0):
 
-        if len(self.pos) == 2:
-            other_agent = self.random.choice(self.model.schedule.agents)
-            if(other_agent == TrashAgent):
-                other_agent.destroy += 1
+            if(self.pos[0] != self.movingToX):
+                
+                if(self.pos[0] <= self.movingToX):
+                    new_pos = (self.pos[0] + 1, self.pos[1])
 
-    def step(self):
-        self.cleanCell()
-        self.move()
+                else:
+
+                    new_pos = (self.pos[0] - 1, self.pos[1])
+
+            if(self.pos[1] != self.movingToY):
+
+                if(self.pos[1] <= self.movingToY):
+
+                    new_pos = (self.pos[0], self.pos[1] + 1)
+
+                else:
+
+                    new_pos = (self.pos[0], self.pos[1] + -1)
+
+            if(self.pos[1] == self.movingToY and self.pos[0] == self.movingToX):
+                self.carrying = 1
+                self.pickUp()
+
+        #carry the box back to the stack
+        if(self.carrying == 1):
+
+            if(self.pos[0] != 0):
+
+                new_pos = (self.pos[0] - 1, self.pos[1])
+
+            else:
+
+                if(self.pos[1] != 0):
+
+                    new_pos = (self.pos[0], self.pos[1] - 1)
+
+        self.model.grid.move_agent(self,new_pos)
 
 
-class TrashAgent(mesa.Agent):
+    def pickUp(self):
+        if(self.carrying == 0):
+            print()
 
-    def __init__(self, unique_id, model):
-        super().__init__(unique_id, model)
-        self.destroy = 0
+    def dropOff(self):
+        self.carrying = 0
 
-    def step(self):
-        if (self.destroy >= 1):
-            self.destroy(self)
+    def stay(self):
+        print("Staying")
 
-
-
-
-class CleaningModel(mesa.Model):
-
-    def __init__(self, NumCleaners, NumTrash, width, height):
-        self.boxCoords = [] 
-        self.num_agents = NumCleaners
-        self.num_box = NumTrash
-        self.grid = mesa.space.MultiGrid(width, height, True)
-        self.schedule = mesa.time.RandomActivation(self)
-
-        #setup agents on grid
-        for i in range(self.num_agents):
-            a = CleanerAgent(i, self)
-            self.schedule.add(a)
-            self.grid.place_agent(a, (1, 1))
-
-        #setup boxes
-        for i in range(self.num_box):
-            b = TrashAgent(i + self.num_box, self)
-            self.schedule.add(b)
-            z = self.grid.find_empty()
-            self.boxCoords.append(z)
-            self.grid.place_agent(b, (z))
-
-    def step(self):
-        self.schedule.step()
